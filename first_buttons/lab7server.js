af2308629c37cf52edf0d6af0cf3e557e6c7812d
tszhonglau <lauxx265@morris.umn.edu>
2017-11-01 16:09:04 -0500

    Added comments

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -9,25 +9,29 @@ port = process.env.PORT || 1337;
 
 var buttons = [];
 
+//This function is responsible for sending a query to the database and receive all the data from credentials.user.till_buttons,
+//queryResults is all the entry from the table. 
 var getButtonsInfo = function(){
   var sql = "SELECT * FROM " + credentials.user + ".till_buttons";
-  queryResults = dbf.query(mysql.format(sql));
-  return(queryResults);
+  return dbf.query(mysql.format(sql));
 }
 
+//This function is responsible for filling in the result from the above function, and populate the map buttons. It returns buttons.
 var fillInButtonsArray = function(result){
   buttons = result;
   return(buttons);
 }
 
+
+//This is implemented so that the functions are executed in order. Each method is returning a Promise which makes sure it finishes first
+//before starting another. 
 dbf=getButtonsInfo()
 .then(fillInButtonsArray)
 .then(dbf.releaseDBF);
 
-//var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
-
+//This is sending the buttons map response back to the client
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){ // handles the /buttons API
-  res.send(buttons);
+  	res.send(buttons);
 });
 app.listen(port);
