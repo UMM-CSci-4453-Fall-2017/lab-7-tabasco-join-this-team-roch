2fa4c63b9aa87768ced94cb5b5b514bd6883033b
rocherno <31316871+rocherno@users.noreply.github.com>
2017-11-18 01:45:25 -0600

    Fixed "app.js" which got contaminated by a merge.

--- public/public/app.js
+++ public/public/app.js
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-var express=require('express'),
-app = express(),
-port = process.env.PORT || 1337;
-
-app.use(express.static(__dirname + '/public'));
-app.listen(port);
-=======
-var app = angular.module('MyTutorialApp', []);
->>>>>>> af2308629c37cf52edf0d6af0cf3e557e6c7812d
+var app = angular.module('MyTutorialApp',[]);
